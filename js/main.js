dd300512529581cabf142e64f15ed79c88e594a6
kelseywhallon <kelsey.whallon@gmail.com>
2020-10-06 09:11:03 -0400

    updated new question in question box

--- js/main.js
+++ js/main.js
@@ -5,42 +5,72 @@ let playGame = false
 //set score at 0
 let score = 0
 //set timer to 60 seconds
-let timer = 10
+let time = 10;
 
+const gameContainer = document.querySelector('.game-container')
 
 
 //create start game button to initiate game
 const startGameButton = document.querySelector('.start-reset')
 startGameButton.addEventListener('click', startGame = () => {
-    if(playGame === true) {
-    //if game is already being played reload to start page
-    document.reload()
-} else {
-    //when game is initiated 
-    //change start button to "reset game"
-    document.querySelector('.start-reset').innerHTML = "Reset Game"
-    //start the timer
-    // startTimer()
-    
-    //show level 
+    if (playGame === true) {
+        //if game is already being played reload to start page
+        document.reload()
+    } else {
+        //when game is initiated 
+        //change start button to "reset game"
+        document.querySelector('.start-reset').innerHTML = "Reset Game"
+        //start the timer
+        // startTimer()
+        setTime()
+        //show level 
 
-    //show timer
-    
-    //generate a question and answers
-    // newQA()
-    
-}
+        //show timer
+
+        //generate a question and answers
+        newQA()
+    }
 })
 
 
-
 //timer function
+function setTime(timeRemaining){
+    //set the interval
+    time = timeRemaining
+    const timeInterval = setInterval(() => {
+        //create a check to stop at 0
+        if (time === 0) {
+            //clear the interval
+            clearInterval(timeInterval)
+            //remove eventListerner on start button?
+
+            //increase the level player is currently on
+            // level++
+
+        } else {
+            //decrement timer--
+            time--
+            console.log(time)
+        }   
+    },1000) 
+    const updateTime = document.querySelector('.time-remaining')
+    updateTime.innerHTML = `Time: ${time}s`  
+    gameContainer.appendChild.updateTime
+}
 
 //change level to 1 once game is started
 
 
 //fill question box with 2 random numbers
-
+function newQA() {
+    let num1 = Math.floor(Math.random() * 9)
+    let num2 = Math.floor(Math.random() * 9)
+    //define the correct answer
+    let correctAnswer = num1 * num2
+    document.querySelector('.question-box').innerHTML = `${num1} x ${num2}`
+    //define correct square of correct answer
+    let correctSquare = Math.floor(Math.random() * 4)
+}
 //fill answer boxes with random numbers
 //make sure one of these numbers is the correct answer
 //shuffle the position of the correct answer each round
